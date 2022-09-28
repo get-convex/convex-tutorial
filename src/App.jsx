96568681a173b018bf71c13d7ddd919f034d8dad
Tom Ballinger <tom@convex.dev>
2022-09-28 16:59:23 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { useState } from "react";
-// @snippet start listMessages
-// @snippet start importHooks
 import { useMutation, useQuery } from "../convex/_generated/react";
-// @snippet end importHooks
 
 export default function App() {
   const messages = useQuery("listMessages") || [];
 
   const [newMessageText, setNewMessageText] = useState("");
-  // @snippet start sendMessage
-  // @snippet start sendMessageHook
   const sendMessage = useMutation("sendMessage");
-  // @snippet end sendMessageHook
 
   const [name] = useState(() => "User " + Math.floor(Math.random() * 10000));
   function handleSendMessage(event) {
@@ -19,15 +13,12 @@ export default function App() {
     sendMessage(newMessageText, name);
     setNewMessageText("");
   }
-  // @snippet end sendMessage
-  // @snippet end listMessages
   return (
     <main>
       <h1>Convex Chat</h1>
       <p className="badge">
         <span>{name}</span>
       </p>
-      {/* @snippet start renderMessages */}
       <ul>
         {messages.map(message => (
           <li key={message._id}>
@@ -37,7 +28,6 @@ export default function App() {
           </li>
         ))}
       </ul>
-      {/* @snippet end renderMessages */}
       <form onSubmit={handleSendMessage}>
         <input
           value={newMessageText}
