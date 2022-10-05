f56d77a6eae1043945fe37bc4a45acb753583bf2
Tom Ballinger <tom@convex.dev>
2022-10-05 09:20:51 -0700

    update

--- convex/listMessages.js
+++ convex/listMessages.js
@@ -1,5 +1,5 @@
 import { query } from "./_generated/server";
 
 export default query(async ({ db }) => {
-  return await db.table("messages").collect();
+  return await db.query("messages").collect();
 });
--- convex/sendMessage.js
+++ convex/sendMessage.js
@@ -2,5 +2,5 @@ import { mutation } from "./_generated/server";
 
 export default mutation(({ db }, body, author) => {
   const message = { body, author };
-  db.insert("messages", message);
+  await db.insert("messages", message);
 });
