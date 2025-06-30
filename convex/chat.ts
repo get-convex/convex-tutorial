3996049846629c69de97ea89561b35e70e41c6e2
Mike Cann <mike.cann@gmail.com>
2025-06-30 09:25:56 +0800

    part 2 done

--- convex/chat.ts
+++ convex/chat.ts
@@ -1,5 +1,6 @@
-import { mutation, query } from "./_generated/server";
+import { internalAction, mutation, query } from "./_generated/server";
 import { v } from "convex/values";
+import { api, internal } from "./_generated/api";
 
 export const sendMessage = mutation({
   args: {
@@ -12,6 +13,13 @@ export const sendMessage = mutation({
       user: args.user,
       body: args.body,
     });
+
+    if (args.body.startsWith("/wiki")) {
+      const topic = args.body.slice(args.body.indexOf(" ") + 1);
+      await ctx.scheduler.runAfter(0, internal.chat.getWikipediaSummary, {
+        topic,
+      });
+    }
   },
 });
 
@@ -24,3 +32,25 @@ export const getMessages = query({
     return messages.reverse();
   },
 });
+
+export const getWikipediaSummary = internalAction({
+  args: { topic: v.string() },
+  handler: async (ctx, args) => {
+    const response = await fetch(
+      "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" +
+        args.topic
+    );
+
+    const summary = getSummaryFromJSON(await response.json());
+
+    await ctx.scheduler.runAfter(0, api.chat.sendMessage, {
+      user: "Wikipedia",
+      body: summary,
+    });
+  },
+});
+
+function getSummaryFromJSON(data: any) {
+  const firstPageId = Object.keys(data.query.pages)[0];
+  return data.query.pages[firstPageId].extract;
+}
